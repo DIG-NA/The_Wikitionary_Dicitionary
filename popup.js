c062239958e0438da0cc2f58650e30dba10ab5dc
DIGNA <xoyenterprise@gmail.com>
2026-04-04 14:53:23 +0400

    - added the audio btn to browser_action

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+const content= document.getElementById("content");
+
 document.getElementById("btn").addEventListener("click", async () => {
   const word = document.getElementById("input").value.trim();
   if (!word) return;
@@ -8,11 +10,12 @@ document.getElementById("btn").addEventListener("click", async () => {
   document.getElementById("content").innerHTML =
     htmlString;
 
+    // const content= document.getElementById("content");
+     content.scrollTop = 0;
 });
 
 
 document.getElementById("input").addEventListener("keydown", async (e) => {
-
   if (e.key == "Enter") {
     e.preventDefault();
     e.stopPropagation();
@@ -27,7 +30,22 @@ document.getElementById("input").addEventListener("keydown", async (e) => {
     document.getElementById("content").innerHTML =
       htmlString;
 
-
+    // const content= document.getElementById("content");
+    content.scrollTop = 0;
   }
-
 })
+
+document.getElementById("audiobtn").addEventListener("click", () => {
+  try {
+    browser.runtime.sendMessage({
+      action: "playTTS",
+      text: input.value,
+      language: "en"
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+addRippleEffect(document.getElementById("audiobtn"));
+addRippleEffect(document.getElementById("btn"));
